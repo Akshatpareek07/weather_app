dc014e8f87c504f998e69402610d8b1725b0c7d5
Akshatpareek07 <pareekakshat07@gmail.com>
2023-09-20 11:16:37 +0530

    weather api giving place temp-c and temp-f

---
+++ script.js
@@ -0,0 +1,16 @@
+const place=window.prompt("eneter you location");
+
+const body = document.querySelector('body');
+      
+async function getWeather() {
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=db1523dad67a486ebf184235231809&q=${place}`, {mode: 'cors'});
+  const weather = await response.json();
+  const weth=document.createElement('p');
+//   weth.textContent=`${weather}`;
+//   body.appendChild(weth);
+console.log(weather["location"]["name"]);
+console.log(weather["current"]["temp_f"]);
+console.log(weather["current"]["temp_c"]);
+}
+
+getWeather();
